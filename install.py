f21cb5e4ef48fc704a06ddfa05d3b8613ba0b3c6
camenduru <54370274+camenduru@users.noreply.github.com>
2023-03-05 15:47:45 +0300

    kohya_ss library

--- install.py
+++ install.py
@@ -3,7 +3,9 @@ from modules import scripts
 
 sd_scripts_repo_commit_hash = os.environ.get('SD_SCRIPTS_COMMIT_HASH', "f0ae7eea950b93b1550dc34a782da3bafda2a8c0")
 sd_scripts_repo = os.environ.get('SD_SCRIPTS_REPO', "https://github.com/camenduru/sd-scripts")
-launch.git_clone(sd_scripts_repo, os.path.join(scripts.basedir(), "extensions", "stable-diffusion-webui-trainer", "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
+sd_scripts_dir = os.path.join(scripts.basedir(), "extensions", "stable-diffusion-webui-trainer", "sd-scripts")
+launch.git_clone(sd_scripts_repo, sd_scripts_dir, "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
+launch.run_pip(f"install {sd_scripts_dir}", "kohya_ss library requirements for diffusers extension")
 
 launch.run_pip("install diffusers==0.13.1", "diffusers==0.13.1 requirements for diffusers extension")
 launch.run_pip("install transformers==4.26.1", "transformers==4.26.1 requirements for diffusers extension")
