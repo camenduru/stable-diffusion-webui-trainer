4757766b711cc0bdb47ee668c46e08cdc38a36a7
camenduru <54370274+camenduru@users.noreply.github.com>
2023-03-05 12:34:13 +0300

    up

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
-import launch, os
+import os, launch, modules
 
 sd_scripts_repo_commit_hash = os.environ.get('SD_SCRIPTS_COMMIT_HASH', "f0ae7eea950b93b1550dc34a782da3bafda2a8c0")
 sd_scripts_repo = os.environ.get('SD_SCRIPTS_REPO', "https://github.com/camenduru/sd-scripts")
-git_clone(sd_scripts_repo, os.path.join(modules.scripts.basedir(), "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
+launch.git_clone(sd_scripts_repo, os.path.join(modules.scripts.basedir(), "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
