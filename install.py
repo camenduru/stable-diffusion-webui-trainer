cfa56bd46478ac697c16dd63826476cb2978345a
camenduru <54370274+camenduru@users.noreply.github.com>
2023-03-05 12:56:02 +0300

    trainer

--- install.py
+++ install.py
@@ -3,7 +3,7 @@ from modules import scripts
 
 sd_scripts_repo_commit_hash = os.environ.get('SD_SCRIPTS_COMMIT_HASH', "f0ae7eea950b93b1550dc34a782da3bafda2a8c0")
 sd_scripts_repo = os.environ.get('SD_SCRIPTS_REPO', "https://github.com/camenduru/sd-scripts")
-launch.git_clone(sd_scripts_repo, os.path.join(scripts.basedir(), "extensions", "stable-diffusion-webui-train", "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
+launch.git_clone(sd_scripts_repo, os.path.join(scripts.basedir(), "extensions", "stable-diffusion-webui-trainer", "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
 
 launch.run_pip("install diffusers==0.13.1", "diffusers==0.13.1 requirements for diffusers extension")
 launch.run_pip("install transformers==4.26.1", "transformers==4.26.1 requirements for diffusers extension")
