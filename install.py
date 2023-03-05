db030248ecd9ac0f7f5b140c6b005e34546478ec
camenduru <54370274+camenduru@users.noreply.github.com>
2023-03-05 12:45:15 +0300

    extensions

--- install.py
+++ install.py
@@ -3,4 +3,4 @@ from modules import scripts
 
 sd_scripts_repo_commit_hash = os.environ.get('SD_SCRIPTS_COMMIT_HASH', "f0ae7eea950b93b1550dc34a782da3bafda2a8c0")
 sd_scripts_repo = os.environ.get('SD_SCRIPTS_REPO', "https://github.com/camenduru/sd-scripts")
-launch.git_clone(sd_scripts_repo, os.path.join(scripts.basedir(), "stable-diffusion-webui-train", "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
+launch.git_clone(sd_scripts_repo, os.path.join(scripts.basedir(), "extensions", "stable-diffusion-webui-train", "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
