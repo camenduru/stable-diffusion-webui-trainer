937e4d39776723f46c254560446e581eb59c00ac
camenduru <54370274+camenduru@users.noreply.github.com>
2023-03-05 12:48:07 +0300

    run_pip

--- install.py
+++ install.py
@@ -4,3 +4,10 @@ from modules import scripts
 sd_scripts_repo_commit_hash = os.environ.get('SD_SCRIPTS_COMMIT_HASH', "f0ae7eea950b93b1550dc34a782da3bafda2a8c0")
 sd_scripts_repo = os.environ.get('SD_SCRIPTS_REPO', "https://github.com/camenduru/sd-scripts")
 launch.git_clone(sd_scripts_repo, os.path.join(scripts.basedir(), "extensions", "stable-diffusion-webui-train", "sd-scripts"), "SD Scripts", sd_scripts_repo_commit_hash)
+
+launch.run_pip("install diffusers==0.13.1", "diffusers==0.13.1 requirements for diffusers extension")
+launch.run_pip("install transformers==4.26.1", "transformers==4.26.1 requirements for diffusers extension")
+launch.run_pip("install ftfy==6.1.1", "ftfy==6.1.1 requirements for diffusers extension")
+launch.run_pip("install accelerate==0.16.0", "accelerate==0.16.0 requirements for diffusers extension")
+launch.run_pip("install bitsandbytes==0.37.0", "bitsandbytes==0.37.0 requirements for diffusers extension")
+launch.run_pip("install safetensors==0.2.8", "safetensors==0.2.8 requirements for diffusers extension")
